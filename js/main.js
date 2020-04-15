10da9056d7b099b6690b26becc14d9c9fca51924
Ranga Rao Manne <manne.rangarao@gmail.com>
2020-04-15 09:28:30 +0530

    getting the data dynamically from json to html through js file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,45 @@
-fetch("data/data.json").then(response=>{return response.json()}).then(data=>{console.log(data);profileInfo(data.profiles)})
+fetch("data/data.json").then(response=>{
+	return response.json()
+}).then(data=>{
+	profileInfo(data.profiles);
+}).catch(e=>{
+	console.error("Error");
+})
+
+var parent=document.getElementById("root");
 var profileInfo=info=>{
-	info.map(i=>{
-		console.log(i.profileInfo.name);
-		console.log(i.profileInfo.mobile);
+	info.map((i,index)=>{
+		var child= document.createElement("article");
+		child.classList.add("child");
+
+		// Name
+		var name=document.createElement("h2");
+		name.textContent=i.profileInfo.name;
+		name.style.color="#FF0000";
+		name.style.textShadow="3px 3px 3px #ddd";
+		child.append(name);
+
+		// Email
+		var email=document.createElement("a");
+		email.textContent=i.profileInfo.email;
+		email.href="mailto:"+i.profileInfo.email;
+		email.classList.add("email");
+		child.append(email);
+
+		// Mobile
+		var mobile=document.createElement("a");
+		mobile.textContent=i.profileInfo.mobile;
+		mobile.href="tel:"+i.profileInfo.mobile;
+		mobile.classList.add("mobile");
+		child.append(mobile);
+
+		var button=document.createElement("a");
+		button.classList.add("btn");
+		button.href="resume.html?id="+index;
+		button.textContent="view profile";
+		child.append(button);
+
+		parent.append(child);
+		// console.log(i.profileInfo.mobile);
 	})
-	}
+}
